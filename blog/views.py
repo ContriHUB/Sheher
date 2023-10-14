9b98b6854dde2301a613950caf11a194f306b092
Nandika-A <nandikaagrawal610@gmail.com>
2023-10-14 15:41:41 +0530

    Changed theme of the website

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
-    DetailView,
     CreateView,
     UpdateView,
     DeleteView
