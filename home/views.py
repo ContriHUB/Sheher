4d12bec99efd9e1a457c0bdd7072abed7742e0dc
Manan-Arora31 <manan31102002@gmail.com>
2023-10-27 00:45:37 +0530

    Corrected cards frontend

--- home/views.py
+++ home/views.py
@@ -22,8 +22,8 @@ def homepage(request):
             all_reviews.append(reviews)
             latitude = places.position.latitude
             longitude = places.position.longitude
-            #weather_info=get_weather_data(latitude,longitude)
-            #places.weather_info=weather_info  #adding weather info to place object
+            weather_info=get_weather_data(latitude,longitude)
+            places.weather_info=weather_info  #adding weather info to place object
         for reviews_qs in all_reviews:
             rating = 0
             count = 0
